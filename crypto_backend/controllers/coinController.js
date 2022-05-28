96f279bd882af0832f1d18d932e08a4617b6d093
Caleb <calebley@hotmail.com>
2022-05-28 18:25:16 +0800

    tidy up code

--- crypto_backend/controllers/coinController.js
+++ crypto_backend/controllers/coinController.js
@@ -5,19 +5,31 @@ const router = express.Router()
 const isAuth = require("../middleware/isAuth")
 
 const userSchema = require("../models/User")
-const favouritecoinSchema = require("../models/Favouritecoin")
+const favouriteCoinSchema = require("../models/Favouritecoin")
 
-//Add new coin to favourite list ====================================
+//Get all coins on watchlist
+router.get("/:user_id", async (req, res) => {
+    await favouriteCoinSchema.find({userid: req.params.user_id})
+    .then(coin => {
+        res.json(coin)
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    })
+})
+
+
+//Add new coin to watchlist ====================================
 router.post("/:coin_id/:user_id", async (req, res) => {
   
     try {
         const user = await userSchema.findById(req.params.user_id).select("-password")
 
-        console.log(req.body.userId)
-        console.log(req.body.coinUuid)
-        const newCoin = new favouritecoinSchema({
-            userId: req.body.userId,
-            coinUuid: req.body.coinUuid
+        console.log("id",req.body.id)
+        console.log(req.body.cryptoInfo.uuid)
+        const newCoin = new favouriteCoinSchema({
+            userId: req.body.id,
+            coinUuid: req.body.cryptoInfo.uuid
         })
 
         console.log(newCoin)
--- crypto_backend/models/Favouritecoin.js
+++ crypto_backend/models/Favouritecoin.js
@@ -7,6 +7,7 @@ const favouriteCoinSchema = new Schema({
     },
     coinUuid: {
         type: String,
+        
 
     }
 })
