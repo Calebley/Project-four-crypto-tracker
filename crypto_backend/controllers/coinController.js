7f7fbee5fc1bd133d2ea773cde445a8c2ec417f9
Caleb <calebley@hotmail.com>
2022-06-01 23:17:06 +0800

    tidy up code

--- crypto_backend/controllers/coinController.js
+++ crypto_backend/controllers/coinController.js
@@ -9,26 +9,54 @@ const favouriteCoinSchema = require("../models/Favouritecoin")
 
 //Get all coins on watchlist
 router.get("/:user_id", async (req, res) => {
-    await favouriteCoinSchema.find({userid: req.params.user_id})
-    .then(coin => {
-        res.json(coin)
-    })
-    .catch(err => {
-        res.status(400).json(err)
-    })
+    await favouriteCoinSchema.find({ userid: req.params.user_id })
+        .then(coin => {
+            res.json(coin)
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
 })
 
 //Add new coin to watchlist ====================================
 router.post("/:coin_id/:user_id", async (req, res) => {
-  
+
     console.log("body", req.body)
 
     try {
         const user = await userSchema.findById(req.params.user_id).select("-password")
 
-        console.log("id",user)
-        
+        //Create coin watchlist id
+        function coinIdGenerator() {
+
+            this.length = 8;
+            this.timestamp = +new Date;
+
+            let _getRandomInt = function (min, max) {
+                return Math.floor(Math.random() * (max - min + 1)) + min;
+            }
+
+            this.generate = function () {
+                let ts = this.timestamp.toString();
+                let parts = ts.split("").reverse();
+                let id = "";
+
+                for (let i = 0; i < this.length; ++i) {
+                    let index = _getRandomInt(0, parts.length - 1);
+                    id += parts[index];
+                }
+
+                return id;
+            }
+        }
+
+
+        console.log("id", user)
+
+        const create_id = new coinIdGenerator()
+        const watchlistId = create_id.generate()
         const newCoin = new favouriteCoinSchema({
+            watchlistId: watchlistId,
             coinUuid: req.body.coin,
             userId: req.body.user
         })
@@ -37,6 +65,17 @@ router.post("/:coin_id/:user_id", async (req, res) => {
         newCoin.save()
     } catch (err) {
         res.status(500).send("Server error")
+    }
+})
+
+//Delete a coin =====================================================
+router.delete("/delete/:watchlist_id", async(req, res) => {
+    try{
+        console.log(req.params.watchlist_id)
+        await favouriteCoinSchema.remove({watchlistId: req.params.watchlist_id})
+        res.status(200).send("success")
+    } catch (error) {
+        res.status(500).send("server error")
     }
 })
 
--- crypto_backend/models/Favouritecoin.js
+++ crypto_backend/models/Favouritecoin.js
@@ -2,11 +2,15 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const favouriteCoinSchema = new Schema({
+    watchlistId: {
+        type: Number
+    },
     userId: {
         type: String
     },
     coinUuid: {
         type: String,
+        unique: true
     }
 })
 
